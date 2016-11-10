689ac7046763e015d31d959f0062eb3fffb06bc9
zhihua-ch <zhihua_ch@yahoo.com>
2016-11-10 17:13:22 +0800

    problem

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 int main(int argc, char *argv[]) {
 	FILE *SrcFp, *TgtFp, *ConFp;
 	
-	char line[100];
-	char ConWds[100];
+	char line[300];
+	char ConWds[300];
 	char Ent[2];
 	
-	int i, x, index, size, lastsize;
+	int i, x, index, size, lastsize, totalLen;
 	long int loc;
 	/*initial character */
-	for(i=0;i<100;i++) {
+	for(i=0;i<300;i++) {
 		line[i] = NULL;
 		ConWds[i] = NULL;
 	}
@@ -22,12 +22,12 @@ int main(int argc, char *argv[]) {
 	Ent[0] = 0x0a;
 	
 	//printf("This is DEV C++ application.\n");
-	SrcFp = fopen("cut.txt", "r+");
+	SrcFp = fopen("String.txt", "r+");
 	
 	if(SrcFp==NULL) printf("open file failed.\n");
 	else printf("open Src file\n");
 	
-	ConFp = fopen("tgt.txt", "r");
+	ConFp = fopen("cnt.txt", "r");//ascii txt file is OK
 	if(ConFp == NULL) printf("open Content file failed \n");
 	else printf("open content file\n");
 	
@@ -42,9 +42,9 @@ int main(int argc, char *argv[]) {
 	else{
 		printf("meet source file end\n");
 	}*/
-	for(index=0;index<3;index++){
+	for(index=0;index<47;index++){
 	
-		if(fgets(line, 90, SrcFp)){
+		if(fgets(line, 200, SrcFp)){
 			i =0;
 		
 			while(line[i] != '=') {
@@ -59,7 +59,7 @@ int main(int argc, char *argv[]) {
 	
 	
 #if 1	
-		if(fgets(ConWds, 80, ConFp)){
+		if(fgets(ConWds, 200, ConFp)){
 		/*x = 0;
 		do{
 			printf("%c", TgtWds[x]);}
@@ -77,8 +77,9 @@ int main(int argc, char *argv[]) {
 			printf("Content line size is %d\n", size);
 			i++;
 			strncpy(&line[i], ConWds, (size-lastsize));//need get content size
-		
-			lastsize = size;
+		    totalLen = size - lastsize + i-1;
+		    
+			lastsize = size;// correct content size
 		
 		//printf("content index end is %d, total line length is %d\n", x, i);
 		
@@ -88,13 +89,13 @@ int main(int argc, char *argv[]) {
 				printf("%c", line[i]);
 			}while(line[i++]);
 		
-		}else printf("Target file is empty\n");
+		}else printf("Content file is empty\n");
 	
 	/*fseek(SrcFp, loc, SEEK_SET);
 	//fputs(line, SrcFp);
 	fwrite(line, 1, 15, SrcFp);*/
 	
-		fwrite(line, 1, i-1, TgtFp);
+		fwrite(line, 1, totalLen, TgtFp);//i-1
 		//1 add "enter"; 2 size
 		//fwrite(Ent, 1, 1, TgtFp);
 	

b069b6a16b8cdb315f93d9ead31b823fad5e1726
zhihua-ch <zhihua_ch@yahoo.com>
2016-11-10 15:10:23 +0800

    correct
    for 3 valid lines

--- main.c
+++ main.c
@@ -8,14 +8,18 @@ int main(int argc, char *argv[]) {
 	
 	char line[100];
 	char ConWds[100];
+	char Ent[2];
 	
-	int i, x;
+	int i, x, index, size, lastsize;
 	long int loc;
 	/*initial character */
 	for(i=0;i<100;i++) {
 		line[i] = NULL;
 		ConWds[i] = NULL;
 	}
+	lastsize = 0;
+	//Ent[0] = 0x0d;
+	Ent[0] = 0x0a;
 	
 	//printf("This is DEV C++ application.\n");
 	SrcFp = fopen("cut.txt", "r+");
@@ -26,6 +30,9 @@ int main(int argc, char *argv[]) {
 	ConFp = fopen("tgt.txt", "r");
 	if(ConFp == NULL) printf("open Content file failed \n");
 	else printf("open content file\n");
+	
+	TgtFp = fopen("target.txt", "w");
+	
 	/*if(fgets(line, 90, SrcFp)){
 		i = 0;
 		do{
@@ -35,20 +42,24 @@ int main(int argc, char *argv[]) {
 	else{
 		printf("meet source file end\n");
 	}*/
-	if(fgets(line, 90, SrcFp)){
-		i =0;
+	for(index=0;index<3;index++){
+	
+		if(fgets(line, 90, SrcFp)){
+			i =0;
 		
-		while(line[i] != '=') {
-			i++;
+			while(line[i] != '=') {
+				i++;
 		//	printf("i is %d", i);
-		}
+			}	
 		
-		printf("index is %d\n", i);
-	}else printf("Source file open to empty.\n");
+		//	printf("index is %d\n", i);
+		}else printf("Source file open to empty.\n");
 	
-	loc = ftell(SrcFp);
+	//loc = ftell(SrcFp);
 	
-	if(fgets(ConWds, 10, ConFp)){
+	
+#if 1	
+		if(fgets(ConWds, 80, ConFp)){
 		/*x = 0;
 		do{
 			printf("%c", TgtWds[x]);}
@@ -60,26 +71,35 @@ int main(int argc, char *argv[]) {
 			line[i++] = TgtWds[x++];
 			printf("i is %d\n", i);
 		}*/
-		i++;
-		strncpy(&line[i], ConWds, 4);//need get content size
+			//x=0; size=0;
+			//while(ConWds[x++] != 0x0d) size++;
+			size = ftell(ConFp);
+			printf("Content line size is %d\n", size);
+			i++;
+			strncpy(&line[i], ConWds, (size-lastsize));//need get content size
 		
+			lastsize = size;
 		
 		//printf("content index end is %d, total line length is %d\n", x, i);
 		
 		/*show result line*/
-		i = 0;
-		do{
-			printf("%c", line[i]);
-		}while(line[i++]);
+			i = 0;
+			do{
+				printf("%c", line[i]);
+			}while(line[i++]);
 		
-	}else printf("Target file is empty\n");
+		}else printf("Target file is empty\n");
 	
 	/*fseek(SrcFp, loc, SEEK_SET);
 	//fputs(line, SrcFp);
 	fwrite(line, 1, 15, SrcFp);*/
-	TgtFp = fopen("target.txt", "w");
 	
-	fwrite(line, 1, 15, TgtFp);
+		fwrite(line, 1, i-1, TgtFp);
+		//1 add "enter"; 2 size
+		//fwrite(Ent, 1, 1, TgtFp);
+	
+}
+#endif	
 	fclose(TgtFp);
 	fclose(SrcFp);
 	fclose(TgtFp);
